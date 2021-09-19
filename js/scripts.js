5963502621e7599f28900a422abd2b3310b94d69
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-09-19 13:11:06 +0300

    changes all the required numerical inputs to integers as needed in jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 // CONSTRUCTROR FOR ORDER PLACEMENT.
-function Orders(pizza_name, pizza_size, crust, topping, numberOfPizzas){
-  this.pizza_name = pizza_name;
-  this.pizza_size = pizza_size;
-  this.crust = crust;
-  this.topping = topping;
+function Orders(pizzaName, pizzaSize, costOfPizzaSize, crustName, crustCost, selectedTopping, costOfSelectedTopping, numberOfPizzas){
+  this.pizzaName = pizzaName;
+  this.pizzaSize = pizzaSize;
+  this.costOfPizzaSize = costOfPizzaSize;
+  this.crustName = crustName;
+  this.crustCost = crustCost;
+  this.selectedTopping = selectedTopping;
+  this.costOfSelectedTopping = costOfSelectedTopping;
   this.numberOfPizzas = numberOfPizzas;
 }
 // CONSTRUCTOR FOR DELIVERY FORM.
@@ -24,16 +27,22 @@ $(document).ready(function() {
 
 
     var selectedPizzaName = $("#pizza-type option:selected").text();
-    var selectedPizzaSize = parseInt($("#size").val());
-    var selectedCrust = parseInt($("#crust").val());
+    var selectedPizzaSize = $("#size option:selected").text();
+    var CostOfSelectedPizzaSize = parseInt($("#size option:selected").val());
+    var selectedCrust = $("#crust option:selected").text();
+    var costOfSelectedCrust = parseInt($("#crust option:selected").val());
     var selectedToppings = new Array();
-    $('input[name = "topping"]:checked').each(function(){
-      selectedToppings.push(this.value);
+    $('input[name = "topping"]:checked').each(function(){ //EDIT HERE TO DISPLAY THE NAMES OF THE CHOOSEN TOPPINGS.
+      selectedToppings.push(this.text);
     })
-    //  parseInt($(".topping").val());
+    var costOfSelectedToppings = new Array();
+    $('input[name = "topping"]:checked').each(function(){
+      costOfSelectedToppings.push(this.value);
+    });
+    var costOfSelectedToppings = costOfSelectedToppings.map(Number);
     var selectedQuantity = parseInt($("#numberOfPizzas").val());
 
-    var newOrder = new Orders(selectedPizzaName, selectedPizzaSize, selectedCrust, selectedToppings, selectedQuantity);
+    var newOrder = new Orders(selectedPizzaName, selectedPizzaSize, CostOfSelectedPizzaSize, selectedCrust, costOfSelectedCrust, selectedToppings, costOfSelectedToppings, selectedQuantity);
     alert(newOrder);
     console.log(newOrder)
     // alert(selectedPizzaName);
