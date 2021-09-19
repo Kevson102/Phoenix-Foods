92d24a96d5c5351e15e96ee5328c07f2d3f736ad
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-09-19 09:56:36 +0300

    adds the jQuery function for collecting form inputs

---
+++ js/other.js
@@ -0,0 +1,5 @@
+var selectedPizzaName = document.getElementById('pizza-type').text();
+var selectedPizzaSize = document.getElementById('#size').val();
+var selectedCrust = document.getElementById("#crust").val();
+var selectedToppings = document.getElementsByClassName(".topping").val();
+var selectedQuantity = document.getElementById("#numberOfPizzas").val();
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // CONSTRUCTROR FOR ORDER PLACEMENT.
-function Orders(pizza_name, pizza_size, crust){
+function Orders(pizza_name, pizza_size, crust, topping, numberOfPizzas){
   this.pizza_name = pizza_name;
   this.pizza_size = pizza_size;
   this.crust = crust;
-  this.topping = [];
+  this.topping = topping;
+  this.numberOfPizzas = numberOfPizzas;
 }
 // CONSTRUCTOR FOR DELIVERY FORM.
 function Delivery(firstName, secondName, idNumber, phoneNumber){
@@ -13,3 +14,35 @@ function Delivery(firstName, secondName, idNumber, phoneNumber){
   this.idNumber = idNumber;
   this.address = [];
 }
+
+
+
+// USER INTERFACE
+$(document).ready(function() {
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+
+
+    var selectedPizzaName = $("#pizza-type option:selected").text();
+    var selectedPizzaSize = parseInt($("#size").val());
+    var selectedCrust = parseInt($("#crust").val());
+    var selectedToppings = new Array();
+    $('input[name = "topping"]:checked').each(function(){
+      selectedToppings.push(this.value);
+    })
+    //  parseInt($(".topping").val());
+    var selectedQuantity = parseInt($("#numberOfPizzas").val());
+
+    var newOrder = new Orders(selectedPizzaName, selectedPizzaSize, selectedCrust, selectedToppings, selectedQuantity);
+    alert(newOrder);
+    console.log(newOrder)
+    // alert(selectedPizzaName);
+
+    // alert(selectedPizzaSize);
+    // alert(typeof(selectedPizzaSize));
+
+    // alert(selectedCrust.text());
+  })
+  // $("ul#your-order").append("<li><span class ='contact'>"+ newOrder + "</span></li>");
+
+})
